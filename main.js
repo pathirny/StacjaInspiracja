d64324fb550e487e7ba8f6b81b4d4d3233cac732
Patryk Hirny <pathirny@googlemail.com>
2023-10-31 11:54:37 +0000

    changed email verification to smtpjs

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ let expanded = document.getElementById("add-expanded");
 function expand() {
   expanded.classList.toggle("add-topic-expanded-open");
 }
+
+function sendEmail() {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "pathirny@googlemail.com",
+    Password: "password",
+    To: "pathirny@googlemail.com",
+    From: "you@isp.com",
+    Subject: "This is the subject",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
+}
